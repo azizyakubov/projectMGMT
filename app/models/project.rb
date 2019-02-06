3fcd3aa37acca79e2a9530b44d911c5854263185
Aziz Yakubov <azizy15@gmail.com>
2019-02-06 12:11:49 -0800

    finished activity panel

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,8 @@ class Project < ApplicationRecord
   belongs_to :user
 
 
-  accepts_nested_attributes_for :team 
+  accepts_nested_attributes_for :team
+
+  include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user }
 end
