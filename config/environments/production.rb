0417db3d25a0d4f9c61cb0663c1a8d627b7a5d46
Aziz Yakubov <azizy15@gmail.com>
2019-02-19 14:15:45 -0800

    configured sendgrid prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ config.webpacker.check_yarn_integrity = false
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  onfig.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'projectmgmt.heroku.com' }
   ActionMailer::Base.smtp_settings = {
